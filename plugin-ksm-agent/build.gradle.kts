0afa59fac56e86427a7258bc4a572905d3935cdc
Ivan Dimov <78815270+idimov-keeper@users.noreply.github.com>
2024-09-26 18:20:27 -0500

    Update dependencies to latest versions

--- plugin-ksm-agent/build.gradle.kts
+++ plugin-ksm-agent/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("jvm")
-  kotlin("plugin.serialization") version "1.9.24"
-  id("io.github.rodm.teamcity-agent") version "1.5.3"
+  kotlin("plugin.serialization") version "2.0.20"
+  id("io.github.rodm.teamcity-agent") version "1.5.5"
 }
 
 teamcity {
@@ -19,8 +19,8 @@ repositories {
 dependencies {
   implementation(project(":plugin-ksm-common"))
 
-  implementation("com.keepersecurity.secrets-manager:core:16.6.4")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+  implementation("com.keepersecurity.secrets-manager:core:16.6.5")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
   testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
   testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 
@@ -29,7 +29,7 @@ dependencies {
 
   testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.assertj:assertj-core:3.25.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+  testImplementation("org.assertj:assertj-core:3.26.3")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.1")
 }
--- plugin-ksm-common/build.gradle.kts
+++ plugin-ksm-common/build.gradle.kts
@@ -8,13 +8,13 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
-  implementation("com.keepersecurity.secrets-manager:core:16.6.4")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
+  implementation("com.keepersecurity.secrets-manager:core:16.6.5")
 
   compileOnly("org.jetbrains.teamcity:common-api:${rootProject.extra["teamcityVersion"]}")
 
   testImplementation("org.jetbrains.teamcity:server-api:${rootProject.extra["teamcityVersion"]}")
-  testImplementation("org.assertj:assertj-core:3.25.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+  testImplementation("org.assertj:assertj-core:3.26.3")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.1")
 }
